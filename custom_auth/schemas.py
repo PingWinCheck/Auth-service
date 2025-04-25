c5f57cec1c2a31a869420147e3b46e5d1c6d3a32
PingWinCheck <ankolt@yandex.ru>
2025-04-26 00:41:06 +0300

    add token schema

--- custom_auth/schemas.py
+++ custom_auth/schemas.py
@@ -9,3 +9,9 @@ class UserBaseSchema(BaseModel):
 
 class UserCreateSchema(UserBaseSchema):
     password: Annotated[str, Field(min_length=8)]
+
+
+class Token(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str = 'Bearer'
